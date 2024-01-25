de38cc9950a5513cf6f7106cd316172fd0e7cbf4
Anand Mohan Sharma <puraskaarwebteam@gmail.com>
2024-01-26 04:47:15 +0530

    updated modules in todo-app

--- 41-react/todo-app/src/components/counter/Counter.jsx
+++ 41-react/todo-app/src/components/counter/Counter.jsx
@@ -1,91 +1,30 @@
 import { useState } from 'react'
+import CounterButton from './CounterButton'
 import './Counter.css'
 
 export default function Counter(){
 
     const [count, setCount] = useState(0)
 
-    function incrementCounterFunction(){
-        setCount(count + 1)
+    function incrementCounterParentFunction(by){
+        setCount(count+by)
+    }
+    
+    function decrementCounterParentFunction(by){
+        setCount(count-by)
     }
 
-    function decrementCounterFunction(){
-        setCount(count - 1)
-    }
-
-    function incrementCounterBy2Function(){
-        setCount(count + 2)
-    }
-
-    function decrementCounterBy2Function(){
-        setCount(count - 2)
-    }
-
-    function decrementCounterBy5Function(){
-        setCount(count - 5)
-    }
-
-    function incrementCounterBy5Function(){
-        setCount(count + 5)
-    }
-
-    function reset(){
+    function Reset(){
         setCount(0)
     }
 
-
     return (
-        <div className="Counter">
-            <span className="count">{count}</span>
-            <div>
-                <div>
-
-                    <button className="counterButton"
-                            onClick={incrementCounterFunction}>
-                            +1
-                    </button>
-
-                    <button className="counterButton"
-                            onClick={decrementCounterFunction}>
-                            -1
-                    </button>
-
-                </div>
-                <div>
-
-                    <button className="counterButton"
-                        onClick={incrementCounterBy2Function}>
-                            +2
-                    </button>
-
-                    <button className="counterButton"
-                            onClick={decrementCounterBy2Function}>
-                            -2
-                    </button>
-
-                </div>
-                <div>
-
-                    <button className="counterButton"
-                        onClick={incrementCounterBy5Function}>
-                        +5
-                    </button>
-
-                    <button className="counterButton"
-                        onClick={decrementCounterBy5Function}>
-                        -5
-                    </button>
-
-                </div>
-                <div>
-
-                    <button className='reset'
-                        onClick={reset}>
-                            Reset
-                    </button>
-
-                </div>
-            </div>
-        </div>
+        <>
+            <CounterButton incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction} />
+            <CounterButton by={2} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <div className="totalCount">{count}</div>
+            <button className='reset' onClick={Reset}> Reset </button>
+        </>
     )
 }
